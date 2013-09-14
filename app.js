db54c1c08ce93e290fcbd8514f80055518929613
Ubuntu <ubuntu@ip-10-202-10-164.ec2.internal>
2013-09-14 14:33:39 +0000

    added call to containers

--- app.js
+++ app.js
@@ -6,9 +6,13 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
+  , Buffer = require('buffer').Buffer
   , path = require('path')
   , uuid = require('uuid')
   , exec = require("child_process").exec;
+require("shellscript").globalize();
+
+var port = 5555;
 
 var app = express();
 
@@ -30,7 +34,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function(req, res) {
+    res.render("terminal");
+});
 
 var server = require('http').createServer(app);
 server.listen(app.get('port'), function(){
@@ -40,15 +46,43 @@ server.listen(app.get('port'), function(){
 var io = require('socket.io').listen(server, { log: false })
 
 io.sockets.on("connection", function(socket) {
-    var createContainer = "sudo docker run -d pythonenv"
-    exec(createContainer, function(err, stdout, stderr) {
-        socket.id = stdout;
-        console.log("the guys id is: " + socket.id);
-    });    
+    port += 1;
+    socket.port = port;
+    var createContainer = "sudo docker run -p " + port + ":3000 -d pythonenv"
+    socket.id = $(createContainer);
+    console.log(socket.id);
+        //socket.container = io.connect("http://localhost:" + socket.port);    
     
-    socket.on("hello", function(data) {
+    socket.on("code", function(data) {
         console.log(data);
+        var payload = JSON.stringify(data);
+        var options = {
+            host: 'localhost',
+            port: socket.port,
+            path: '/',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': payload.length
+            }
+        };
+        var req = http.request(options, function(res) {
+            res.setEncoding('utf-8');
+            var responseString = '';
+             res.on('data', function(data) {
+                 responseString += data;
+             });
+             res.on('end', function() {
+                 var result = JSON.parse(responseString);
+                 console.log(result);
+                 console.log("why is this not emitting?");
+                 socket.emit("result", result);
+             });
+        });
+        req.write(payload);
+        req.end();
     });
+
     socket.on("disconnect", function() {
         var killContainer = "sudo docker kill " + socket.id;
         exec(killContainer, function(err, stdout, stderr) {
