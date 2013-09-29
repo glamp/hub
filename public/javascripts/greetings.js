93270d68b6df2110ee0fb776e8b2920b4dff2f8c
Ubuntu <ubuntu@ip-10-196-191-205.ec2.internal>
2013-09-29 17:16:44 +0000

    working w/ ruby, python, and r

--- public/javascripts/greetings.js
+++ public/javascripts/greetings.js
@@ -1,3 +1,6 @@
 var rgreeting = 'R version 3.0.0 (2013-04-03) -- "Masked Marvel"\nCopyright (C) 2013 The R Foundation for Statistical Computing\nPlatform: x86_64-apple-darwin10.8.0 (64-bit)\n\nR is free software and comes with ABSOLUTELY NO WARRANTY.\nYou are welcome to redistribute it under certain conditions.\nType \'license()\' or \'licence()\' for distribution details.\n\n  Natural language support but running in an English locale\n\nR is a collaborative project with many contributors.\nType \'contributors()\' for more information and\n\'citation()\' on how to cite R or R packages in publications.\n\nType \'demo()\' for some demos, \'help()\' for on-line help, or\n\'help.start()\' for an HTML browser interface to help.\nType \'q()\' to quit R.\n';
 
 var pygreeting = 'Python 2.7.3 (default, Apr 10 2013, 06:20:15)\n[GCC 4.6.3] on linux2\nType "help", "copyright", "credits" or "license" for more information.\n';
+
+var rubygreeting = '';
+
